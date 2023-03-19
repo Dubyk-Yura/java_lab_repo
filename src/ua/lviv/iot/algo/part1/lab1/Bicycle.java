94780f9de66fd52282775a181f8f5ae01d3b33ba
Dubyk_Yura <dubykjurij@gmail.com>
2023-03-19 14:53:21 +0200

    test v.3

--- src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
+++ src/ua/lviv/iot/algo/part1/lab1/Bicycle.java
@@ -58,6 +58,7 @@ public class Bicycle {
         //create second object with instance
         bicycles[3] = getInstance();
 
+
         for (Bicycle bicycle : bicycles) {
             System.out.println(bicycle);
         }
